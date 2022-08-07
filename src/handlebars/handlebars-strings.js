0ed1ed0420789eae62939ca233e7714866fa9a26
Austin Heller <me@aheller.com>
2022-08-07 15:19:04 +0000

    String parser: Don’t nix unprocessed strings

--- src/handlebars/handlebars-strings.js
+++ src/handlebars/handlebars-strings.js
@@ -7,9 +7,13 @@ export default function parseStrings(props) {
       newContent = newContent.replace(regex, meta[key]);
    });
    // remove any unprocessed strings
+   // NOTE: disabled because if you have a valid string inside a nested block, Surfgreen will strip it, which we don't want
+   /*
    const unprocessedStringRegex = /{{(\w+\d*)?}}/g;
-   if( newContent.match(unprocessedStringRegex) ) {
+   const unprocessedStrings = newContent.match(unprocessedStringRegex);
+   if( unprocessedStrings ) {
       newContent = newContent.replace(unprocessedStringRegex, '');
    }
+   */
    return newContent;
 }
